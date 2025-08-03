8010660995df293b5406d8558eb551eefaff9822
Sidhanta <sidhantanayak980@gmail.com>
2025-08-03 20:30:24 +0530

    add deploy link in frontend

--- visualization-dashboard-frontend/src/services/api.jsx
+++ visualization-dashboard-frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/insights'; // Update if deployed
+const API_URL = 'https://visualization-dashboard-backend-ehib.onrender.com/api/insights'; // Update if deployed
 
 export const fetchInsights = async () => {
   const res = await axios.get(API_URL);
